6fe6fe9fb67c5eff7a26f641659cf1de154bdd79
Uula Ranta <uula.ranta@gmail.com>
2017-03-09 00:34:52 +0200

    Implement POST to shoot squares

--- tt_bs/routes.py
+++ tt_bs/routes.py
@@ -14,7 +14,8 @@ def hello_world():
 def get_game(game_name):
     game = get_game_or_abort(game_name)
     teams = [team.as_dict() for team in Team.query.filter_by(game_name=game_name).all()]
-    squares = [square.as_dict() for square in Square.query.filter_by(game_name=game_name).filter(Square.shot_team_name != None).all()]
+    squares = [square.as_dict() for square in Square.query.filter_by(game_name=game_name)
+        .filter(Square.shot_team_name != None).all()]
     return jsonify(name=game.name, teams=teams, squares=squares)
 
 
@@ -22,17 +23,29 @@ def get_game(game_name):
 def shoot(game_name):
     game = get_game_or_abort(game_name)
     try:
-        team = request.args['team']
-        x = request.args['x']
-        y = request.args['y']
-    except KeyError:
+        team_name = request.args['team']
+        x = int(request.args['x'])
+        y = int(request.args['y'])
+    except (KeyError, ValueError):
         abort(400)
         return
 
+    team = Team.query.filter_by(name=team_name).first()
+    if not team:
+        abort(404, "Team '{}' not found for this game".format(team_name))
+
     square = Square.query.filter_by(x=x, y=y).first()
     if square:
-        if square.ship_team and square.shot_team:
-            pass
+        if square.ship_team:
+            if not square.shot_team:
+                square.shot_team = team
+                db.session.add(square)
+                db.session.commit()
+                return jsonify(square=square.as_dict(), hit=True), 200
+            else:
+                return jsonify(square=square.as_dict(), hit=False), 200
+    return "", 204
+
 
 def get_game_or_abort(game_name):
     game = Game.query.filter_by(name=game_name).first()
@@ -41,7 +54,8 @@ def get_game_or_abort(game_name):
     else:
         return game
 
-@app.errorhandler(404)
-def error_404(error):
-    response = jsonify(error=404, message=error.description)
-    return response, 404
+
+def get_team_or_abort(game_name, team_name):
+    team = Game.query.filter_by(name=team_name, game_name=game_name).first()
+    if not team:
+        abort(404, 'Team with name \'{}\' was not found'.format(team_name))
