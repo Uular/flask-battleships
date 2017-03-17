de8789e4685e2df1bdc974c43cfe57f011c9594b
Uula Ranta <uula.ranta@gmail.com>
2017-03-17 14:54:26 +0200

    Game setup

--- game_setup.py
+++ game_setup.py
@@ -1,6 +1,6 @@
 GAME_NAME = 'titeta2017'
-WIDTH = 1000
-HEIGHT = 1000
+WIDTH = 512
+HEIGHT = 512
 
 TEAMS = [("otit", "#11CC11"), ("tite", "#1111CC"), ("tik", "#AAAA22"), ("digit", "#CC1111"), ("cluster", "#22AAAA")]
-SHIPS = [6] * 3 + [5] * 4 + [4] * 5 + [3] * 7 + [2] * 10
+SHIPS = [7] * 53 + [6] * 201 + [5] * 300 + [4] * 400 + [3] * 500 + [2] * 800
